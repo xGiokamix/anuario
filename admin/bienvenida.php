6b01ba29121f8deb8db169d04b90e80154c1b4a3
Rubén Geovanni Velasco Alvarez <wolf.madhatter@gmail.com>
2024-10-10 11:33:32 -0600

    Se agregan vinculos a las paginas usuarios.php y alumnos.php

--- admin/bienvenida.php
+++ admin/bienvenida.php
@@ -341,6 +341,9 @@
                         </button>
                     </div>
                 </div>
+                <div>
+                    <a href="usuarios.php">Usuarios</a>
+                    <a href="alumnos.php">Alumnos</a></div>
 
                 <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
 
