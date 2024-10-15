60db1711fada8129f9829b3513d5e85a543f1dba
Rubén Geovanni Velasco Alvarez <wolf.madhatter@gmail.com>
2024-10-15 08:24:20 -0600

    se crea eliminar_usuario.php, Sql para eliminar usuario

---
+++ admin/eliminar_usuario.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+//Valida la sesion
+if(!isset($_SESSION['usuario'])){
+    header("Location: index.php");
+    exit();
+}
+   include_once("../conf.php"); 
+    $con = mysqli_connect(HOST,DBUSER,PASS,DB);
+    if(isset($_GET['id'])){
+        $sql="delete from usuarios where usuario='".$_GET['id']."'";
+        $res=mysqli_query($con,$sql);
+    }
+        header("Location: usuarios.php"); 
+        exit;
+?>
